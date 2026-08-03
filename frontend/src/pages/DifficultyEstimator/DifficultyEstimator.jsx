bd6ae720e423f89d0527af52fcafb806ec59d439
Mayur Sahare <mayursahare2024@gmail.com>
2026-08-03 15:45:12 +0530

    UX: Fix Syntax Error in DifficultyEstimator.jsx

--- frontend/src/pages/DifficultyEstimator/DifficultyEstimator.jsx
+++ frontend/src/pages/DifficultyEstimator/DifficultyEstimator.jsx
@@ -102,7 +102,7 @@ const DifficultyEstimator = () => {
 
         </div>
                 {result && (
-
+          <>
           <div className="space-y-8">
 
             {/* Main Analysis */}
@@ -395,7 +395,8 @@ const DifficultyEstimator = () => {
 
             </div>
 
-          </div>
+
+          </>
         )}
 
       </div>
